9e4b599157a9f96988e389d2382963d12a08585a
Pallab dutta <121430112+pallab1999@users.noreply.github.com>
2025-03-22 14:45:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ codes[0].focus();
 
 codes.forEach((code,idx)=>{
     code.addEventListener("input",(event)=>{
-        if(event.target.value.length===1 && idx<codes.length-1){
+        if(event.target.value.length === 1 && idx < codes.length - 1){
             codes[idx+1].focus();
         }
     })
     code.addEventListener("keydown",(event)=>{
-        if(event.key==='Backspace'){
+        if(event.key === 'Backspace'){
             if(idx > 0){
+                event.target.value = '';
                 codes[idx-1].focus();
             }
         }
